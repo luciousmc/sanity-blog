19075ec591e3859abce088fe34376f1ac60b49b0
Marlon Clay <luciousmc@icloud.com>
2022-01-26 14:14:39 -0800

    feat: add Hero section to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import Head from 'next/head'
 
 import Header from '../components/Header'
+import HeroBanner from '../components/HeroBanner'
+
 export default function Home() {
   return (
-    <div className="">
+    <div className="mx-auto max-w-7xl">
       <Head>
         <title>Blogging Site</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+      <HeroBanner />
     </div>
   )
 }
